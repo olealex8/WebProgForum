fd277eebb8ae6ff9e2c17e867ebfe921bfc28cf5
Ole Alexander <117496574+olealex8@users.noreply.github.com>
2022-11-07 16:33:37 +0700

    Add files via upload

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,8 @@
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-center sticky-top" style="display: flex;">
+    <a class="navbar-brand" href="#">-Web Programming Forum-</a>
+
+    <!-- <h7 style="color:#FFFFFF">2440063315 - Ole Alexander</h7> -->
+</nav>
+
+
