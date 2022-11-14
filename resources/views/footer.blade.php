ad855e7e5c798586d7781de05990aaf7d132888f
Ole Alexander <117496574+olealex8@users.noreply.github.com>
2022-11-14 17:49:00 +0700

    Add files via upload

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,18 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start footer-dark bg-dark text-muted sticky-top">
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(52,58,64,255);">
+    
+    <div style="color: #FFFFFF;">
+        © 2022 Copyright: All Rights Reserved 
+        <br>
+        <br>
+        
+        <p style="font-size: 12px;">2440063315 - Ole Alexander</p>
+        
+    </div>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
